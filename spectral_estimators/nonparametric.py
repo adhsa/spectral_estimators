ae1c1f4c4802bdeb18398b80068d115cecc6ba2d
adhsa <asatna13@gmail.com>
2018-11-03 23:12:26 +0100

    Implemented beamformer along with the first test method

--- spectral_estimators/nonparametric.py
+++ spectral_estimators/nonparametric.py
@@ -19,4 +19,8 @@ def beamformer(R, A):
 
     """
 
-    pass
+    phi = np.zeros(A.shape[1])
+    for i in range(A.shape[1]):
+        a = A[:, i].reshape(-1, 1)
+        phi[i] = np.real(np.squeeze(np.dot(np.conj(a).T, np.dot(R, a))))
+    return phi
--- spectral_estimators/tests/test_nonparametric.py
+++ spectral_estimators/tests/test_nonparametric.py
@@ -3,3 +3,31 @@ import numpy as np
 
 from spectral_estimators.covariance import covariance_matrix
 from spectral_estimators.nonparametric import beamformer
+from spectral_estimators.utils import steering_matrix, single_radar_target
+
+
+class TestBeamformer(unittest.TestCase):
+
+    def test_single_peak(self):
+        """
+        This method sweeps through several peaks and tests if the beamformer gives a correct estimate.
+
+        TODO: Make this into a decorator
+        """
+
+        n_sensors = 12
+        theta_grid = np.linspace(-90, 90, 720)
+        A = steering_matrix(theta_grid, n_sensors)
+
+        thetas = np.linspace(-80, 80, 20)
+        for theta in thetas:
+            data = single_radar_target(theta, n_sensors)
+            data += 0.1 * (np.random.randn(*data.shape) + 1j * np.random.randn(*data.shape)) / np.sqrt(2)
+
+            R = covariance_matrix(data)
+            phi = beamformer(R, A)
+
+            ind = np.argmax(phi)
+            estimated = theta_grid[ind]
+            error = np.linalg.norm(theta - estimated)
+            self.assertGreater(0.5, error)
